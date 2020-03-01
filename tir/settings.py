b11e10edded233e36206b6d2952b5c639e5f73da
kvfi <mail@ouafi.net>
2020-03-01 16:17:37 +0100

    Add pytest to depedencies

--- tir/settings.py
+++ tir/settings.py
@@ -1,10 +1,12 @@
 import logging
+import os
 import warnings
 
 from yaml import Dumper, FullLoader, load, dump
 
 logger = logging.getLogger(__name__)
 
+_DEFAULT_CONF_FILE = 'tir.yml'
 _DEFAULT_SETTINGS = {
     'build_dir': 'html',
     'visuals': {
@@ -14,21 +16,19 @@ _DEFAULT_SETTINGS = {
 
 
 def load_settings(override: dict = None) -> dict:
-    try:
-        with open('tir.yml', 'r', encoding='UTF-8') as f:
-            content = f.read()
-            cfg = load(content, Loader=FullLoader) or {}
-    except FileNotFoundError:
-        warnings.warn('No configuration file was found.')
+    cfg = {}
+    if os.path.isfile(_DEFAULT_CONF_FILE):
+        with open(_DEFAULT_CONF_FILE, 'r', encoding='UTF-8') as file:
+            content = file.read()
+            logger.debug(_DEFAULT_CONF_FILE, content)
+            cfg = load(content, Loader=FullLoader)
 
-    merged_cfg = {**cfg, **_DEFAULT_SETTINGS}
-
-    print(merged_cfg)
+    merged_cfg = {**_DEFAULT_SETTINGS, **cfg}
 
     if override is not None:
-        merged_cfg = {**merged_cfg, **override}
+        merged_cfg = {**override, **merged_cfg}
 
-    with open('tir.yml', 'w+', encoding='UTF-8') as f:
+    with open(_DEFAULT_CONF_FILE, 'w+', encoding='UTF-8') as f:
         f.write(dump(merged_cfg, Dumper=Dumper))
 
     return merged_cfg
