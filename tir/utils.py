98675dca5284d96917bc589635698ce8ceddbc01
kvfi <mail@ouafi.net>
2020-07-25 12:18:51 +0100

    Added n bits hash for strings

--- tir/utils.py
+++ tir/utils.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 import platform
 import shutil
@@ -108,3 +109,7 @@ def copytree(src, dst, symlinks=False, ignore=None):
         else:
             if not os.path.exists(d) or os.stat(s).st_mtime - os.stat(d).st_mtime > 1:
                 shutil.copy2(s, d)
+
+
+def n_bit_hash(string: str, n: int) -> int:
+    return int(hashlib.blake2b(string.encode('utf-8')).hexdigest(), 16) % 10 ** n
