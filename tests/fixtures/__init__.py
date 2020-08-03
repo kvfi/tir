56cac32ef1ebfd74519ea8472bc52446a1bc3bfb
kvfi <mail@ouafi.net>
2020-08-03 12:59:06 +0100

    Added tests and fixtures

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -1,2 +1,8 @@
+import os
+
+import pytest
 
 
+@pytest.fixture
+def sample_env_file_path():
+    return os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data', '.env-test')
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,13 @@
-import pytest
-
 from tests import temporary_folder
+from tir.utils import load_env
 
 
 def test_simple():
     with temporary_folder() as tf:
-        assert tf is not None, "Folder object is not None"
+        assert tf is not None, 'Folder object is not None'
+
+
+def test_load_env(sample_env_file_path):
+    path = sample_env_file_path
+    env = load_env(path)
+    assert isinstance(env, dict)
